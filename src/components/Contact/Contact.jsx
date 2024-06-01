7b5aba7cf72a45ec0889b8679f435367c5d9c6ee
Lucieabd98 <l.abadia@orange.fr>
2024-06-01 11:44:29 +0200

    added mail and message html

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -39,7 +39,7 @@ const Contact = () => {
             />
             <input
               type="email"
-              name="email"
+              name="mail"
               id="mail"
               required
               placeholder="Mail"
