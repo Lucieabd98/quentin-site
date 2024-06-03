9c65844e4b3bc6dbad3d785661aaeb7fc4ae3e0d
Lucieabd98 <l.abadia@orange.fr>
2024-06-03 16:06:05 +0200

    seo app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import Container from "@mui/material/Container";
 import { Element } from "react-scroll";
+import { Helmet } from "react-helmet";
 
 // Impport des composants
 
@@ -18,6 +19,33 @@ library.add(faXmark, faPaperPlane);
 function App() {
   return (
     <>
+      <Helmet>
+        <meta
+          name="description"
+          content="Le site de Quentin Abadia, producteur son et compositeur, vous plonge dans un univers musical unique. Découvrez son portfolio diversifié et explorez ses créations sonores originales, allant de l'identité musicale d'entreprises et de produits au montage audio de projets artistiques. Contactez Quentin pour collaborer sur votre prochain projet musical et donnez vie à vos idées avec sa passion et son expertise."
+        />
+        <meta property="og:title" content="Quentin Abadia - Producteur son" />
+        <meta
+          property="og:description"
+          content="Le site de Quentin Abadia, producteur son et compositeur, vous plonge dans un univers musical unique. Découvrez son portfolio diversifié et explorez ses créations sonores originales, allant de l'identité musicale d'entreprises et de produits au montage audio de projets artistiques. Contactez Quentin pour collaborer sur votre prochain projet musical et donnez vie à vos idées avec sa passion et son expertise."
+        />
+        <meta property="og:url" content="https://quentin-abadia.netlify.app/" />
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:image"
+          content="https://quentin-abadia.netlify.app/imgs/first-sight-site.png"
+        />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Quentin Abadia - Producteur son" />
+        <meta
+          name="twitter:description"
+          content="Le site de Quentin Abadia, producteur son et compositeur, vous plonge dans un univers musical unique. Découvrez son portfolio diversifié et explorez ses créations sonores originales, allant de l'identité musicale d'entreprises et de produits au montage audio de projets artistiques. Contactez Quentin pour collaborer sur votre prochain projet musical et donnez vie à vos idées avec sa passion et son expertise."
+        />
+        <meta
+          name="twitter:image"
+          content="https://quentin-abadia.netlify.app/imgs/first-sight-site.png"
+        />
+      </Helmet>
       <Header />
       <Container>
         <Definitions />
